7a1d2d099b25f0fe6c430b6b36fd6fc929fbbc96
Priytosh Tripathi <digiren.priytosh@gmail.com>
2022-02-03 23:55:13 +0530

    Time: 0 ms (100.00%), Space: 41.6 MB (5.48%) - LeetHub

---
+++ 198-house-robber/198-house-robber.java
@@ -0,0 +1,20 @@
+class Solution 
+{
+    public int rob(int[] A)
+    {
+      int n = A.length;
+        if(n==0)
+            return 0;
+        if(n==1)
+            return A[0];
+      int dp[] = new int[n];
+        
+      dp[0] = A[0];
+      dp[1] = A[0]>A[1]?A[0]:A[1];
+        
+    for(int i=2;i<n;i++)
+     dp[i] = Math.max(A[i]+dp[i-2],dp[i-1]);
+        
+     return dp[n-1];   
+    }
+}
