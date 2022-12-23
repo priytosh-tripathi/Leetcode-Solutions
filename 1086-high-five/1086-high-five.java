757443b7984cf54e67b58e55a1a49846d9dce098
Priytosh Tripathi <digiren.priytosh@gmail.com>
2022-12-23 12:28:53 +0530

    Time: 18 ms (7.12%), Space: 46.6 MB (19.71%) - LeetHub

---
+++ 1086-high-five/1086-high-five.java
@@ -0,0 +1,43 @@
+class Solution 
+{
+    public int[][] highFive(int[][] items) 
+    {
+       HashMap<Integer,ArrayList<Integer>> hm = new HashMap<>();
+       ArrayList<Integer> values = new ArrayList<>();
+       for(int i=0;i<items.length;i++)
+       {
+           ArrayList<Integer> temp;
+           if(hm.containsKey(items[i][0]))
+           {
+               temp = hm.get(items[i][0]);
+               temp.add(items[i][1]);
+           }
+           else{
+               temp = new ArrayList<>();
+               temp.add(items[i][1]);
+               values.add(items[i][0]);
+           }
+           hm.put(items[i][0],temp);
+       }
+       Iterator<Integer> it = values.listIterator();
+        int ans[][] = new int[values.size()][2];
+        int i=0;
+        while(it.hasNext())
+        {
+            int x = it.next();
+            ArrayList<Integer> temp = hm.get(x);
+            Collections.sort(temp, Collections.reverseOrder());
+            int sum = temp.get(0)+temp.get(1)+temp.get(2)+temp.get(3)+temp.get(4);
+            int avg = sum/5;
+            if(x!=0 || avg!=0)
+            {
+            ans[i][0] = x;
+            ans[i][1] = avg; 
+            }
+            i++;
+        }
+        Arrays.sort(ans,(a,b)->a[0]-b[0]);
+        return ans;
+        
+    }
+}
